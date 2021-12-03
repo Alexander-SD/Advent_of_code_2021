3afac822799eec2a6b00efa78680d777be80b40a
Alexander Dudarev <alexanderdudarev@Alexanders-MacBook-Pro.local>
2021-12-03 10:42:32 +0100

    Updated  .gitignore file

--- day1/build/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const calculateLargerMeasurements_1 = require("./calculateLargerMeasurements");
-const puzzleInput_1 = require("./puzzleInput");
-const result = (0, calculateLargerMeasurements_1.calculateLargerMeasurements)(puzzleInput_1.puzzleInput);
-console.log(`Count of larger measurements is ${result}`);
