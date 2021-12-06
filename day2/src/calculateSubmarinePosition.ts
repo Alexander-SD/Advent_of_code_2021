39f76c0788b5286ec71b697bce890e88ba9d49cb
Alexander Dudarev <alexanderdudarev@Alexanders-MacBook-Pro.local>
2021-12-06 02:10:02 +0100

    Fixed solution of the part 2.

--- day2/src/calculateSubmarinePosition.ts
+++ day2/src/calculateSubmarinePosition.ts
@@ -6,12 +6,13 @@ export const calculateSubmarinePosition = (input: string) => {
     let horizontalPosition: number = 0;
     let depth: number = 0;
     let aim: number = 0;
+    let aimDiffer: number = 0;
     
     for (let i = 0; i < inputArray.length - 1; i = i + 2) {
         bufferArray = inputArray.slice(i, i + 2);  
         if (bufferArray[0] === Direction.Forward) {
             horizontalPosition = horizontalPosition + parseInt(bufferArray[1]);
-            depth = depth + (horizontalPosition * aim);
+                depth = depth + (aim * parseInt(bufferArray[1]));
         } else if (bufferArray[0] === Direction.Down) {
             aim = aim + parseInt(bufferArray[1]);
         } else if (bufferArray[0] === Direction.Up) {
